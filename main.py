2553b8bd73ad7d32238b1526367692a534ac4712
tsukasakop <tsukasakop@gmail.com>
2023-07-23 22:40:38 +0900

    FIX: format src

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from functools import singledispatch, reduce
 
 import xmltodict as x2d
 import MeCab
-from neo4j import GraphDatabase, basic_auth
+from neo4j import GraphDatabase
+
 
 @singledispatch
 def get_txt_by_snt(snt: str) -> str:
@@ -14,8 +15,6 @@ def get_txt_by_snt(snt: str) -> str:
 def _(snt: list) -> str:
     txts = (p["#text"] for p in snt)
     return "\n".join(txts)
-    
-    
 
 @singledispatch
 def get_txt_by_ps(ps: dict) -> str:
@@ -26,7 +25,7 @@ def get_txt_by_ps(ps: dict) -> str:
 @get_txt_by_ps.register
 def _(ps: list) -> str:
     txts = (get_txt_by_snt(s) for s in ps)
-    return "\n".join(txts)    
+    return "\n".join(txts)
 
 @singledispatch
 def get_txt_by_para(para: dict) -> str:
@@ -40,14 +39,14 @@ def get_txt_by_para(para: dict) -> str:
 def _(para: list) -> str:
     txts = (get_txt_by_para(p) for p in para)
     return "\n".join(txts)
-    
+
 def get_txt_by_art(art:dict) -> str:
     if "Paragraph" not in art:
         raise Exception("invalid article")
     return get_txt_by_para(art["Paragraph"])
 
-word_type_wl=["名詞", "動詞"]
-def count_words(txt: str) -> dict:    
+def count_words(txt: str) -> dict:
+    word_type_wl=["名詞", "動詞"]
     cnt = {}
     tagger = MeCab.Tagger()
     node = tagger.parseToNode(txt)
@@ -61,78 +60,45 @@ def count_words(txt: str) -> dict:
             cnt[word] = 0
         cnt[word] += 1
         node = node.next
-        
     return cnt
 
 def make_cypher_query(tx, d: dict) -> None:
-    # tx.run("CREATE (n:Test)")
     for art in d:
         query = f"CREATE (n:Article{{name: $name}})"
-        # input(query)
         tx.run(query, name=art)
     terms = list(set(reduce(lambda a,b: a+list(b.keys()), d.values(), [])))
     for term in terms:
         query = f"CREATE (n:Term{{name: $name}})"
-        # input(term)
         tx.run(query, name=term)
     for art, cnt in d.items():
         for term in cnt.keys():
             query = f"MATCH(a:Article),(t:Term) WHERE a.name = '{art}' AND t.name = '{term}' CREATE (a)-[r:USES]->(t)"
-            # input(query)
             tx.run(query)
-    
-            
-        
-        
-    
-    
+
 
 def main():
-    FNAME = "./xml/321CONSTITUTION_19470503_000000000000000/321CONSTITUTION_19470503_000000000000000.xml"
-    with open(FNAME,encoding="utf-8") as fd:
+    # get raw law source
+    fname = "./xml/321CONSTITUTION_19470503_000000000000000/321CONSTITUTION_19470503_000000000000000.xml"
+    with open(fname, encoding="utf-8") as fd:
         txt = fd.read()
 
+    # get aricles
     article_pattern = r"<Article\b[^>]*>.*?</Article>"
     article_matches = re.findall(article_pattern, txt, re.DOTALL)
 
-    # Print the matched content inside the <div> tags
+    # parse and count terms
     d = {}
     for match in article_matches:
         art = x2d.parse(match)["Article"]
         title = art["ArticleTitle"]
         art_txt = get_txt_by_art(art)
         d[title] = count_words(art_txt)
-        # input(match)
 
+    # import data to neo4j
     driver = GraphDatabase.driver('bolt://localhost:7687', auth=('neo4j', 'password'))
     with driver.session() as session:
         session.execute_write(make_cypher_query,d)
-    
-    
-        
 
-
-"""use xmltodict
-doc = x2d.parse()
-body = doc["Law"]["LawBody"]
-print(body["LawTitle"]["#text"])
-
-# Preamble data
-for p in body["Preamble"]["Paragraph"]:
-    #print(p["@Num"])
-    #print(p["ParagraphSentence"]["Sentence"])
-    pass
-
-
-# Main article data
-for chapter in body["MainProvision"]["Chapter"]:
-    print(chapter["ChapterTitle"])
-    for article in chapter["Article"]:
-        print(article)
-        print(article["ArticleTitle"])
-        print(article["Paragraph"])
-        print(get_txt_from_paragraph(article["Paragraph"]))
-"""
 
 if __name__ == "__main__":
     main()
